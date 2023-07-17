1621414abbba7f7fa1e7d4c5e539780e34b7d48f
Prathamesh-Mane <113008851+Prathamesh-Mane@users.noreply.github.com>
2023-07-17 19:36:51 +0530

    removed the location program from script.js and added the program required for advertisement. HTML CSS files are updated for the first and second step

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,15 @@
-/*
- This is js file for getting location from client's device
-*/
-const Http = new XMLHttpRequest();
-function getLocation() {
-    console.log("getLocation Called");
-    var bdcApi = "https://api.bigdatacloud.net/data/reverse-geocode-client"
-
-    navigator.geolocation.getCurrentPosition(
-        (position) => {
-            bdcApi = bdcApi
-                + "?latitude=" + position.coords.latitude
-                + "&longitude=" + position.coords.longitude
-                + "&localityLanguage=en";
-            getApi(bdcApi);
-            //document.getElementById("demo").innerHTML = "In coordinates<br>Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude;
-        },
-        (err) => { getApi(bdcApi); },
-        {
-            enableHighAccuracy: true,
-            timeout: 5000,
-            maximumAge: 0
-        });
-}
-function getApi(bdcApi) {
-    Http.open("GET", bdcApi);
-    Http.send();
-    Http.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-        }
-    };
-}
-
-
-getLocation();
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById('myVideo');
+    var background = document.querySelector('.background');
+  
+    video.addEventListener('ended', function() {
+      // Redirect to Step 2 or trigger the necessary logic for Step 2
+      window.location.href = 'step2.html';
+    });
+  
+    // Pause the video after 30 seconds
+    setTimeout(function() {
+      video.pause();
+      background.style.display = 'none';
+    }, 30000);
+  });
