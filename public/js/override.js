fda59dc7b37b0f45c519cc9e5b289b80bb398cc0
Prathamesh-Mane <113008851+Prathamesh-Mane@users.noreply.github.com>
2023-07-20 18:05:33 +0530

    made some changes in override files

--- public/js/override.js
+++ public/js/override.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    // Get the element with the "background" class
-    var backgroundElement = document.querySelector(".background");
+  // Get the element with the "background" class
+  var backgroundElement = document.querySelector(".background");
 
-    // Remove the child node(s) of the "background" element
-    while (backgroundElement.firstChild) {
-      backgroundElement.removeChild(backgroundElement.firstChild);
-    }
-  });
+  // Remove the child node(s) of the "background" element
+  while (backgroundElement.firstChild) {
+    backgroundElement.removeChild(backgroundElement.firstChild);
+  }
+});
