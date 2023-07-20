e00b3a7c7b6e8bf1154eb5ab0b29c88f77c6fbc5
Prathamesh-Mane <113008851+Prathamesh-Mane@users.noreply.github.com>
2023-07-20 17:59:40 +0530

    Replaced the bg image with gradient and changed the MP4 file in media

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var video = document.getElementById('myVideo');
-    var background = document.querySelector('.background');
-  
-    video.addEventListener('ended', function() {
-      // Redirect to Step 2 or trigger the necessary logic for Step 2
-      window.location.href = 'step2.html';
-    });
-  
-    // Pause the video after 30 seconds
-    setTimeout(function() {
-      video.pause();
-      background.style.display = 'none';
-    }, 30000);
+  var video = document.getElementById('myVideo');
+  var background = document.querySelector('.background');
+
+  video.addEventListener('ended', function() {
+    window.location.href = 'step2.html';
   });
+
+  setTimeout(function() {
+    video.pause();
+    background.style.display = 'none';
+  }, 80000);
+});
