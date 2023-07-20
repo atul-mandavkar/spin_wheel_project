ef9f5b1a0395b93ca7de0773e6c3fd5af9b2cfea
Prathamesh-Mane <113008851+Prathamesh-Mane@users.noreply.github.com>
2023-07-20 19:21:16 +0530

    Deleted override files

--- public/js/override.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  // Get the element with the "background" class
-  var backgroundElement = document.querySelector(".background");
-
-  // Remove the child node(s) of the "background" element
-  while (backgroundElement.firstChild) {
-    backgroundElement.removeChild(backgroundElement.firstChild);
-  }
-});
